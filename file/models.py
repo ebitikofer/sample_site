3277f7d7d7a22066b8983c1bb6f7d6cd116b0978
Eric Bitikofer <e.bitikofer@gmail.com>
2016-12-15 11:45:47 -0500

    Added models, views and migrated to db values for pages and books

--- file/models.py
+++ file/models.py
@@ -2,4 +2,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class File(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    location = models.CharField(max_length=250)
+    rating = models.CharField(max_length=250)
+    type = models.CharField(max_length=250)
+    description = models.CharField(max_length=500)
+    picture0 = models.CharField(max_length=1000)
+    picture1 = models.CharField(max_length=1000)
+    picture2 = models.CharField(max_length=1000)
+    picture3 = models.CharField(max_length=1000)
+    picture4 = models.CharField(max_length=1000)
+
+class Link(models.Model):
+    title = models.CharField(max_length=250)
+    rating = models.CharField(max_length=250)
+    thumbnail = models.CharField(max_length=1000)
+    link = models.CharField(max_length=1000)
--- register/models.py
+++ register/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Link(models.Model):
+    title = models.CharField(max_length=250)
+    rating = models.CharField(max_length=250)
+    thumbnail = models.CharField(max_length=1000)
+    link = models.CharField(max_length=1000)
