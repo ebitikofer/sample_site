ae8e36e4502f7c813a56d557de52e0c84dad53dd
Eric Bitikofer <e.bitikofer@gmail.com>
2016-12-18 12:33:55 -0500

    Detail page now working, index links to detail pages

--- register/views.py
+++ register/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from .models import Link
+from .models import File
 
 def index(request):
     all_links = Link.objects.all() 
     html = ''
     for link in all_links:
         html += '<a href = "' + link.link + '">' + link.title + '</a><br>'
+    return HttpResponse(html)
+
+def detail(request, fid):
+    file = File.objects.get(id=fid)
+    html = file.title + '<br>' + file.author + '<br>' + file.location + '<br>' + file.rating + '<br>' + file.type + '<br><br>' + file.description + '<br>'
     return HttpResponse(html)
